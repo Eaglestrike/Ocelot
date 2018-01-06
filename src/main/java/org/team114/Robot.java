dd96eb6eb9254c2872504f8deb3453dd4fca6263
Aris (Karim) Merchant <wizkarim@gmail.com>
2018-01-06 10:51:39 -0800

    Add empty Robot class

---
+++ src/main/java/org/team114/Robot.java
@@ -0,0 +1,8 @@
+package org.team114;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Main robot class, which acts as the root for ownership and control of the robot.
+ */
+public class Robot extends IterativeRobot {}
---
+++ src/main/java/org/team114/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Top-level package for all robot code. Nothing except {@code Robot.java} should go directly in here.
+ */
+package org.team114;
