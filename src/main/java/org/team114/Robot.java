09520b2f114975df4e270333fa4cb602cb7ec20e
Josh Hejna <josh.hejna@gmail.com>
2018-01-08 17:03:51 -0800

    Iterative Robot Overrides

--- src/main/java/org/team114/Robot.java
+++ src/main/java/org/team114/Robot.java
@@ -1,8 +1,65 @@
 package org.team114;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-
+import org.team114.lib.subsystem.SubsystemManager;
 /**
  * Main robot class, which acts as the root for ownership and control of the robot.
  */
-public class Robot extends IterativeRobot {}
+public class Robot extends IterativeRobot {
+
+    SubsystemManager subsystemManager;
+
+    @Override
+    public void robotInit() {
+        subsystemManager = new SubsystemManager();
+    }
+
+    @Override
+    public void disabledInit() {
+
+    }
+
+    @Override
+    public void autonomousInit() {
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void testInit() {
+
+    }
+
+    @Override
+    public void robotPeriodic() {
+
+    }
+
+    @Override
+    public void disabledPeriodic() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+
+    @Override
+    public void testPeriodic() {
+
+    }
+
+    @Override
+    protected void loopFunc() {
+
+    }
+}
