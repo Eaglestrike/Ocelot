72bfa3b20cfef32673717c402b875614051ede47
Daniel Ghasemfar <youoridiot@gmail.com>
2018-01-09 16:32:08 -0800

    Begin logging

---
+++ src/main/java/org/team114/ocelot/logging/Node.java
@@ -0,0 +1,12 @@
+package org.team114.ocelot.logging;
+
+
+import java.util.UUID;
+
+public class Node {
+
+    public UUID id = UUID.randomUUID();
+    public double timestamp = System.currentTimeMillis();
+
+
+}
