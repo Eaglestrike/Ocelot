51a3c406c35fe9106872c84880b6b67b1cd001de
Rebecca Swernofsky <rswernofsky@gmail.com>
2018-01-19 16:42:28 -0800

    Initial drive subsystem (#11)
    * Add testing, speed, and neutral mode events
    * Use lambdas as event handlers
    * Make a drive talon collection

---
+++ src/main/java/org/team114/ocelot/subsystems/drive/SelfTestEvent.java
@@ -0,0 +1,14 @@
+package org.team114.ocelot.subsystems.drive;
+
+import org.team114.ocelot.event.Event;
+
+/*
+Test to make sure all Talon SRXs have a device ID between 0 and 62
+ */
+public class SelfTestEvent extends DriveEvent {
+
+    public SelfTestEvent() {
+
+    }
+
+}
---
+++ src/main/java/org/team114/ocelot/subsystems/drive/SetSideSpeedEvent.java
@@ -0,0 +1,17 @@
+package org.team114.ocelot.subsystems.drive;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import org.team114.ocelot.event.Event;
+
+public class SetSideSpeedEvent extends DriveEvent {
+
+    public final ControlMode mode;
+    public final double leftspeed;
+    public final double rightspeed;
+
+    public SetSideSpeedEvent(ControlMode mode, double leftspeed, double rightspeed) {
+        this.mode = mode;
+        this.leftspeed = leftspeed;
+        this.rightspeed = rightspeed;
+    }
+}
