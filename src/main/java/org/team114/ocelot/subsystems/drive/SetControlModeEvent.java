1c7b1d2d5002e6e8ee50b1e1b3f4fe5c0a019bcb
Patrick Moore <patmoore@farreach.es>
2018-01-21 18:34:26 -0800

    SetControlModeEvent separate from SetSideSpeedEvent

--- src/main/java/org/team114/ocelot/subsystems/drive/SetControlModeEvent.java
+++ src/main/java/org/team114/ocelot/subsystems/drive/SetControlModeEvent.java
@@ -2,15 +2,11 @@ package org.team114.ocelot.subsystems.drive;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
-public final class SetSideSpeedEvent extends DriveEvent {
+public final class SetControlModeEvent extends DriveEvent {
 
     public final ControlMode mode;
-    public final double leftspeed;
-    public final double rightspeed;
 
-    public SetSideSpeedEvent(ControlMode mode, double leftspeed, double rightspeed) {
+    public SetControlModeEvent(ControlMode mode) {
         this.mode = mode;
-        this.leftspeed = leftspeed;
-        this.rightspeed = rightspeed;
     }
 }
