e36204d87a6828dbf6ce369bd1fa2b0d8efbba2b
Aris (Karim) Merchant <wizkarim@gmail.com>
2018-01-22 23:39:20 -0800

    Replace redundant Action class with plain Runnable
    Actions were really just runnables anyway, and interfaces are preferable
    to abstract classes. Additionally, @Danappelxx needs an action which is
    also a subsystem manager.
    
    Some changes were made to implements the switch. RunnableAction
    is now redundant, but removing it caused a problem with mocking in
    the tests, because lambdas are final and cannot be mocked. In order to
    fix this problem, I created a new class that implements Runnable in
    place of the previous wrapped lambda, and mocked that.

--- src/main/java/org/team114/lib/auto/actions/NullAction.java
+++ src/main/java/org/team114/lib/auto/actions/NullAction.java
@@ -3,7 +3,7 @@ package org.team114.lib.auto.actions;
 /**
  * An action that does absolutely nothing.
  */
-public class NullAction extends Action {
+public class NullAction implements Runnable {
 
     /**
      * Do nothing.
--- src/test/java/org/team114/lib/auto/actions/ParallelActionTest.java
+++ src/test/java/org/team114/lib/auto/actions/ParallelActionTest.java
@@ -4,6 +4,7 @@ import org.junit.*;
 import org.mockito.Mockito;
 
 import java.util.concurrent.CountDownLatch;
+import java.lang.Runnable;
 
 import static org.mockito.Mockito.times;
 
@@ -13,26 +14,33 @@ import static org.mockito.Mockito.times;
 public class ParallelActionTest {
 
     //at class level to avoid triggering timeout
-    private CountDownLatch latch = new CountDownLatch(3);
-    private Runnable runnable = ()  -> {
-        latch.countDown();
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            //TODO error logging
-            System.out.print("Test interrupted: " + e.getMessage());
-        }
-    };
 
-    private Action spy = Mockito.spy(new RunnerAction(runnable));
-    private Action parallel = new ParallelAction(spy, spy, spy);
+    private static class TestRunnable implements Runnable {
+        private CountDownLatch latch = new CountDownLatch(3);
+
+        @Override
+        public void run() {
+            latch.countDown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                //TODO error logging
+                System.out.print("Test interrupted: " + e.getMessage());
+            }
+        }
+    }
+
+    private Runnable runnable = new TestRunnable();
+
+    private Runnable spy = Mockito.spy(runnable);
+    private Runnable parallel = new ParallelAction(spy, spy, spy);
 
     /**
      * Uses a CountDownLatch to test that all actions are run concurrently. If the actions do not
      * run at the same time, the latch will never unlock and the test will halt indefinitely,
      * triggering a timeout.
      */
-    @Test(timeout = 100)
+    @Test(timeout = 500)
     public void testConcurrentExecution() {
 
         parallel.run();
