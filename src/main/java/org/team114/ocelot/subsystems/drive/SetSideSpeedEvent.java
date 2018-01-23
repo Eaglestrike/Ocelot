a60f64f22c3a519f552daacf41ecba6179d551f7
Patrick Moore <patmoore@farreach.es>
2018-01-23 01:56:15 -0800

    remove unneeded import

--- src/main/java/org/team114/ocelot/subsystems/drive/SetSideSpeedEvent.java
+++ src/main/java/org/team114/ocelot/subsystems/drive/SetSideSpeedEvent.java
@@ -1,7 +1,5 @@
 package org.team114.ocelot.subsystems.drive;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-
 public final class SetSideSpeedEvent extends DriveEvent {
 
     private final double leftspeed;
