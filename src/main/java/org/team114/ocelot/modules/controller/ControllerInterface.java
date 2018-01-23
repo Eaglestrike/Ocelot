ceb27ce3525f43c71c788d0fac1b0741f6a916e6
Jasper Meggitt <jasper.meggitt@gmail.com>
2018-01-22 16:20:48 -0800

    Change some methods of controller interface class

--- src/main/java/org/team114/ocelot/modules/controller/ControllerInterface.java
+++ src/main/java/org/team114/ocelot/modules/controller/ControllerInterface.java
@@ -1,76 +1,53 @@
 package org.team114.ocelot.modules.controller;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import org.team114.ocelot.event.Event;
 
 public class ControllerInterface {
 
-    private double lastX_LEFT, lastY_LEFT, lastX_RIGHT, lastY_RIGHT, lastZ;
 
     private Joystick left;
     private Joystick right;
 
-    private static final int LEFT = 0, RIGHT = 1;
-
     public ControllerInterface(int port1, int port2) {
         left = new Joystick(port1);
         right = new Joystick(port2);
-
-
-        lastX_LEFT = this.getX(LEFT);
-        lastY_LEFT = this.getY(LEFT);
-        lastX_RIGHT = this.getX(RIGHT);
-        lastY_RIGHT = this.getY(RIGHT);
-        //lastZ = this.getZ();
     }
 
 
     //Is there really a need for all of these?
-    public double getX(int a) {
-        // true is right, false is left
-        if(a == RIGHT){
-            return right.getX();
-        } else if (a == LEFT){
-            return left.getX();
-        } else {
-            throw new IllegalArgumentException("Not a valid string input");
-        }
+    public double leftX() {
+        return left.getX();
     }
 
-    public double getY(int a) {
-        // true is right, false is left
-        if(a == RIGHT){
-            return right.getY();
-        } else if (a == LEFT){
-            return left.getY();
-        } else {
-            throw new IllegalArgumentException("Not a valid string input");
-        }
+    public double leftY() {
+        return left.getY();
     }
 
-//    public double getZ(){
-//        return r
-//    }
+    public double rightX() {
+        return right.getX();
+    }
 
-    public double getAngleRight() {
+    public double rightY() {
+        return right.getY();
+    }
+
+    public double angleRight() {
         return right.getDirectionDegrees();
     }
 
-
-    public double getMagnitudeLeft() {
-        return getY(LEFT);
+    public double angleLeft() {
+        return left.getDirectionDegrees();
     }
 
-    public boolean getLiftStatus() {
+    public boolean liftStatus() {
         return left.getRawButtonPressed(1);
     }
 
-    public boolean getIntakeStatus() {
+    public boolean intakeStatus() {
         return left.getRawButtonPressed(2);
     }
 
-    public boolean getClimbStatus() {
+    public boolean climbStatus() {
         return right.getRawButtonPressed(1);
     }
 
