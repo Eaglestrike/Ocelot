4a32b1acd0ba460f46c6bcfbc2149d557772a24c
Daniel Ghasemfar <youoridiot@gmail.com>
2018-01-26 16:18:17 -0800

    Remove getAngle methods and make throttle method for SingleController.java

--- src/main/java/org/team114/ocelot/modules/DualController.java
+++ src/main/java/org/team114/ocelot/modules/DualController.java
@@ -5,13 +5,6 @@ import org.team114.lib.util.EdgeDetector;
 
 public class DualController implements Controller {
 
-    private final EdgeDetector startLift = new EdgeDetector(this::startLift);
-    private final EdgeDetector endLift = new EdgeDetector(this::endLift);
-    private final EdgeDetector intake = new EdgeDetector(this::intake);
-    private final EdgeDetector startClimb = new EdgeDetector(this::startClimb);
-    private final EdgeDetector endClimb = new EdgeDetector(this::endClimb);
-    private final EdgeDetector sensitivty = new EdgeDetector(this::sensitivity);
-
     private double velocityConstant = 1;
     private Joystick left;
     private Joystick right;
@@ -21,40 +14,12 @@ public class DualController implements Controller {
         this.right = right;
     }
 
-    public void setSpeed(double a) {
-        velocityConstant = a;
-    }
-
     public double throttle() {
         return left.getY();
     }
 
     public double wheel() {
         return right.getX();
-    }
-
-    public double angleLeft() {
-        return left.getDirectionDegrees();
-    }
-
-    public double angleRight() {
-        return right.getDirectionDegrees();
-    }
-
-    /*
-    This might be changed later depending on what we want speed to be
-    */
-
-    public double speed() {
-        return left.getY() * velocityConstant;
-    }
-
-    /*
-    This might be changed later depending on what we want the angle of the turn to be
-    */
-
-    public double getAngleOfTurn() {
-        return angleRight();
     }
 
     public boolean startLift() {
@@ -81,8 +46,5 @@ public class DualController implements Controller {
         return right.getRawButton(3);
     }
 
-    public boolean sensitivity() {
-        return right.getRawButton(4);
-    }
 }
 
