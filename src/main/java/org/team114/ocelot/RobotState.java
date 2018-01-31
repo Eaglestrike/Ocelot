0c52ab179769fa3ca2717b4812128e306bc59dc3
Patrick Moore <patmoore@farreach.es>
2018-01-30 21:12:19 -0800

    Use AtomicReference to avoid syncronization in RobotState

--- src/main/java/org/team114/ocelot/RobotState.java
+++ src/main/java/org/team114/ocelot/RobotState.java
@@ -2,20 +2,22 @@ package org.team114.ocelot;
 
 import org.team114.ocelot.util.Pose;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class RobotState {
 
     private final RobotRegistry robotRegistry;
-    private Pose latest;
+    private AtomicReference<Pose> latest;
 
     public RobotState(RobotRegistry robotRegistry) {
         this.robotRegistry = robotRegistry;
     }
 
-    public synchronized void addObservation(Pose pose) {
-        this.latest = pose;
+    public void addObservation(Pose pose) {
+        this.latest.set(pose);
     }
 
-    public synchronized Pose getLatestPose() {
-        return this.latest;
+    public Pose getLatestPose() {
+        return this.latest.get();
     }
 }
