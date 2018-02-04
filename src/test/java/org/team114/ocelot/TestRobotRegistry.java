cc70d9054adc6fa33596ee04baaf527e5046739a
Patrick Moore <patmoore@farreach.es>
2018-02-03 18:33:56 -0800

    convert test code to junit tests

--- src/test/java/org/team114/ocelot/TestRobotRegistry.java
+++ src/test/java/org/team114/ocelot/TestRobotRegistry.java
@@ -1,16 +1,18 @@
 package org.team114.ocelot;
 
+import junit.framework.TestCase;
+import org.junit.Test;
 import org.team114.ocelot.settings.RobotSettings;
 
 import java.io.IOException;
 
-public class TestRobotRegistry {
-    public static void main(String[] argv) throws IOException {
+public class TestRobotRegistry extends TestCase {
+    @Test
+    public void test() throws IOException {
         RobotRegistryImpl robotRegistry = new RobotRegistryImpl(new RobotSettings().load());
         int channelA = robotRegistry.getSubRobotRegistry("Drive")
                 .getSubRobotRegistry("left")
                 .getConfiguration().getInt("channelA");
-        System.out.println(channelA);
-
+        assertEquals(5, channelA);
     }
 }
--- src/test/java/org/team114/ocelot/settings/TestRobotSettings.java
+++ src/test/java/org/team114/ocelot/settings/TestRobotSettings.java
@@ -1,21 +1,19 @@
 package org.team114.ocelot.settings;
 
+import junit.framework.TestCase;
+
 import java.io.IOException;
 
-public class TestRobotSettings {
-    public static void main(String[] argv) {
-        try {
-            RobotSettings robotSettings = new RobotSettings().load();
-            RobotSettings.Configuration config = robotSettings.getConfiguration("Drive");
-            boolean b = config.getBoolean("left.reversedDirection");
-            System.out.println(b);
-            int cA = config.getChannelAndRegister("right.channelA");
-            System.out.println(cA);
-            double d = config.getDouble("right.distancePerPulseInFeet");
-            System.out.println(d);
-            System.out.println(robotSettings.getChannelRegistry().toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+public class TestRobotSettings extends TestCase {
+    public void test() throws IOException {
+        RobotSettings robotSettings = new RobotSettings().load();
+        RobotSettings.Configuration config = robotSettings.getConfiguration("Drive");
+        boolean b = config.getBoolean("left.reversedDirection");
+        assertTrue(b);
+        int cA = config.getChannelAndRegister("right.channelA");
+        assertEquals(7, cA);
+        double d = config.getDouble("right.distancePerPulseInFeet");
+        System.out.println(d);
+        System.out.println(robotSettings.getChannelRegistry().toString());
     }
 }
