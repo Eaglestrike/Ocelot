cbb51cc5910be5bc682ed7269e1eb689a67cf588
Rebecca Swernofsky <rswernofsky@gmail.com>
2018-02-07 14:26:19 -0800

    Add limit switches

--- src/main/java/org/team114/ocelot/modules/Lift.java
+++ src/main/java/org/team114/ocelot/modules/Lift.java
@@ -1,6 +1,8 @@
 package org.team114.ocelot.modules;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import org.team114.ocelot.settings.RobotSettings;
@@ -9,19 +11,36 @@ public class Lift {
     private final TalonSRX masterTalon;
     private final TalonSRX slaveTalon;
 
-    private ControlMode controlMode;
+    private final DigitalInput topLimitSwitch;
+    private final DigitalInput bottomLimitSwitch;
+
     private double goalHeight;
 
-    public Lift(TalonSRX masterTalon, TalonSRX slaveTalon) {
+    public Lift(TalonSRX masterTalon, TalonSRX slaveTalon, DigitalInput topLimitSwitch,
+                DigitalInput bottomLimitSwitch) {
         this.masterTalon = masterTalon;
         this.slaveTalon = slaveTalon;
+
+        this.topLimitSwitch = topLimitSwitch;
+        this.bottomLimitSwitch = bottomLimitSwitch;
+        masterTalon.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
+        masterTalon.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen, 0);
     }
 
     //go to height (height in feet)
     // multiply height by constant
     public void goToHeight(double height) {
-        //TODO: think about limit switches (digital inputs)
         //TODO: only count a switch pressed if it's pressed for a certain time
+
+        //as if the limit switches are wired to the talon
+
+        if (topLimitSwitch.get()) {
+            masterTalon.setSelectedSensorPosition(convertFeetToTicks(RobotSettings.MAX_LIFT_HEIGHT), 0, 0);
+        }
+        else if (bottomLimitSwitch.get()) {
+            masterTalon.setSelectedSensorPosition(0, 0, 0);
+        }
+
         masterTalon.set(ControlMode.MotionMagic, convertFeetToTicks(height));
         goalHeight = height;
     }
@@ -48,7 +67,7 @@ public class Lift {
         return (ticks / RobotSettings.ENCODER_TICKS_PER_REVOLUTION) / RobotSettings.CLIMBER_FEET_PER_REVOLUTION;
     }
 
-    public double convertFeetToTicks(double feet) {
-        return RobotSettings.ENCODER_TICKS_PER_REVOLUTION * (feet / RobotSettings.CLIMBER_FEET_PER_REVOLUTION);
+    public int convertFeetToTicks(double feet) {
+        return (int)(RobotSettings.ENCODER_TICKS_PER_REVOLUTION * (feet / RobotSettings.CLIMBER_FEET_PER_REVOLUTION));
     }
 }
