c581b21b3d392c259cdba1406f1b1fb1772e9792
Josh Hejna <josh.hejna@gmail.com>
2018-03-11 15:35:30 -0700

    Implement finish line behavior

--- src/main/java/org/team114/ocelot/util/motion/PathPointList.java
+++ src/main/java/org/team114/ocelot/util/motion/PathPointList.java
@@ -1,54 +1,37 @@
 package org.team114.ocelot.util.motion;
 
 import org.team114.lib.geometry.Point;
-import org.team114.lib.pathgenerator.Path;
-import org.team114.lib.util.Epsilon;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * An used to save the PathComponent list using gson.
+ * Holds information necessary for a Pure-Pursuit controller to follow its path.
  */
 public class PathPointList {
 
-    public List<PathComponent> pathComponentList;
+    public List<PathComponent> mainPathPoints;
+    public final PathComponent goalComponent;
+    public final int goalComponentIndex;
+    public final Point finishVector;
 
-    public PathPointList(List<PathComponent> pathComponentList){
-        this.pathComponentList = pathComponentList;
+
+    public PathPointList(List<PathComponent> pathComponentList, int goalPointIndex, Point finishVector) {
+        this.mainPathPoints = pathComponentList;
+        this.goalComponentIndex = goalPointIndex;
+        this.goalComponent = this.get(goalPointIndex);
+        this.finishVector = finishVector;
+    }
+
+    public boolean isPastFinishLine(Point test) {
+        // if the dot-produce is positive, we're past the line (SO ROBUST)
+        Point goalToTest = new Point(
+                test.x() - goalComponent.getLocation().x(),
+                test.y() - goalComponent.getLocation().y()
+        );
+        return ((finishVector.x() * goalToTest.x()) + (finishVector.y() * goalToTest.y())) > 0;
     }
 
     public PathComponent get(int i) {
-        return this.pathComponentList.get(i);
-    }
-
-    public static PathPointList generate(Path p, double segmentDistance) {
-        if (segmentDistance <= 0.0) {
-            throw new IllegalArgumentException("Segment distance must be positive!");
-        }
-        List<PathComponent> pointList = new ArrayList<>();
-        double tIndex = 0;
-        double cumDistance = 0;
-        Point lastPoint;
-
-        lastPoint = p.getPointAtT(tIndex);
-        pointList.add(new PathComponent(lastPoint, cumDistance));
-
-        while (tIndex < p.length()) {
-            Point dydx = p.dydx(tIndex);
-            if (dydx.x() == 0.0 && dydx.y() == 0.0) {
-                tIndex += Epsilon.EPSILON;
-            } else {
-                double speed = Math.sqrt(Math.pow(dydx.y(), 2) +  Math.pow(dydx.x(), 2));
-                tIndex += segmentDistance / speed;
-            }
-            Point newPoint = p.getPointAtT(tIndex);
-            cumDistance += lastPoint.dist(newPoint);
-            lastPoint = newPoint;
-
-            pointList.add(new PathComponent(newPoint, cumDistance));
-        }
-
-        return new PathPointList(pointList);
+        return this.mainPathPoints.get(i);
     }
 }
--- src/main/java/org/team114/ocelot/util/motion/PurePursuitFactory.java
+++ src/main/java/org/team114/ocelot/util/motion/PurePursuitFactory.java
@@ -4,29 +4,39 @@ import org.team114.lib.geometry.Point;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class PurePursuitFactory {
-
     public static PathPointList loadPath(String pathName) {
         String resourceBasePath = "/org/team114/ocelot/paths/";
-        InputStream inputStream = PurePursuitFactory.class.getResourceAsStream(resourceBasePath + pathName + ".csv");
+        InputStream inputStream = PurePursuitFactory.class.getResourceAsStream(resourceBasePath + pathName + ".114path");
         InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
         BufferedReader reader = new BufferedReader(streamReader);
 
-        List<PathComponent> components = reader.lines()
-            .skip(1) // skip the header line
-            .map(line -> line.split(","))
-            .map(line -> new PathComponent(
-                new Point(
-                    Double.parseDouble(line[0]), // x
-                    Double.parseDouble(line[1]) // y
-                ), Double.parseDouble(line[2]) // distance
-            ))
-            .collect(Collectors.toList());
-
-        return new PathPointList(components);
+        String[] lines = (String[])(reader.lines().toArray());
+        int finishLineVecIndex = lines.length-1;
+        List<PathComponent> path = new ArrayList<>(lines.length-2);
+        int goalPointIndex = 0;
+        for (int i = 1; i < finishLineVecIndex-1; i++) {
+            String[] line = lines[i].split(",");
+            path.add(new PathComponent(
+                    new Point(
+                            Double.parseDouble(line[0]), // x
+                            Double.parseDouble(line[1]) // y
+                    ), Double.parseDouble(line[2]) // distance
+            ));
+            // if we're not in linear-interp land update the goal point
+            if (!Boolean.parseBoolean(line[3])) {
+                goalPointIndex = i;
+            }
+        }
+        String[] vecLine = lines[finishLineVecIndex].split(",");
+        return new PathPointList(path, goalPointIndex, new Point(
+                Double.parseDouble(vecLine[0]), // x
+                Double.parseDouble(vecLine[1]) // y
+        ));
     }
 
     public static PurePursuitController startPurePursuit(PathPointList path, double lookAheadDistance, double finishMargin) {
