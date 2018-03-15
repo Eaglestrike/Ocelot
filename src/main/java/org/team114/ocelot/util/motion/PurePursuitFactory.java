de43d858ba863efe8c3828dbd8a806f0cc3acd3c
Aris (Karim) Merchant <wizkarim@gmail.com>
2018-03-15 15:26:19 -0700

    Refactor and reformat PurePursuitFactory

--- src/main/java/org/team114/ocelot/util/motion/PurePursuitFactory.java
+++ src/main/java/org/team114/ocelot/util/motion/PurePursuitFactory.java
@@ -2,7 +2,9 @@ package org.team114.ocelot.util.motion;
 
 import org.team114.lib.geometry.Point;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,17 +12,18 @@ import java.util.stream.Collectors;
 
 public abstract class PurePursuitFactory {
     public static PathPointList loadPath(String pathName) {
-        String resourceBasePath = "/org/team114/ocelot/paths/";
-        InputStream inputStream = PurePursuitFactory.class.getResourceAsStream(resourceBasePath + pathName + ".114path");
-        InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        BufferedReader reader = new BufferedReader(streamReader);
+        final String resourceBasePath = "/org/team114/ocelot/paths/";
+        final InputStream inputStream = PurePursuitFactory.class.getResourceAsStream(resourceBasePath + pathName + ".114path");
+        final InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        final BufferedReader reader = new BufferedReader(streamReader);
 
-        String[] lines = (String[])(reader.lines().toArray());
-        int finishLineVecIndex = lines.length-1;
-        List<PathComponent> path = new ArrayList<>(lines.length-2);
+        final List<String> lines = reader.lines().collect(Collectors.toList());
+        final int finishLineVecIndex = lines.size() - 1;
+        final List<PathComponent> path = new ArrayList<>(lines.size() - 2);
         int goalPointIndex = 0;
-        for (int i = 1; i < finishLineVecIndex-1; i++) {
-            String[] line = lines[i].split(",");
+
+        for (int i = 1; i < finishLineVecIndex - 1; i++) {
+            String[] line = lines.get(i).split(",", -1);
             path.add(new PathComponent(
                     new Point(
                             Double.parseDouble(line[0]), // x
@@ -32,7 +35,8 @@ public abstract class PurePursuitFactory {
                 goalPointIndex = i;
             }
         }
-        String[] vecLine = lines[finishLineVecIndex].split(",");
+
+        String[] vecLine = lines.get(finishLineVecIndex).split(",", -1);
         return new PathPointList(path, goalPointIndex, new Point(
                 Double.parseDouble(vecLine[0]), // x
                 Double.parseDouble(vecLine[1]) // y
