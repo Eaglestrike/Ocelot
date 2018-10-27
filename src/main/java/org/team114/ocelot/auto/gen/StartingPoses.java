25fcbf697a870f68dd4681700b0e023d35d5d039
Josh Hejna <josh.hejna@gmail.com>
2018-10-27 06:55:40 -0700

    Check-in paths using new starting pose

--- src/main/java/org/team114/ocelot/auto/gen/StartingPoses.java
+++ src/main/java/org/team114/ocelot/auto/gen/StartingPoses.java
@@ -3,7 +3,8 @@ package org.team114.ocelot.auto.gen;
 import org.team114.ocelot.util.Pose;
 
 public class StartingPoses {
-    public static Pose leftSideStart = new Pose(-7.8125002499999985, 1.56250005, 1.5707963267948966, 0.0);
-    public static Pose rightSideStart = new Pose(7.8125002499999985, 1.56250005, 1.5707963267948966, 0.0);
+    public static Pose leftSideStart = new Pose(-9.58333364, 1.56250005, 1.5707963267948966, 0.0);
+    public static Pose rightSideStart = new Pose(9.58333364, 1.56250005, 1.5707963267948966, 0.0);
     public static Pose centerStart = new Pose(0.41666668, 1.56250005, 1.5707963267948966, 0.0);
 }
+
